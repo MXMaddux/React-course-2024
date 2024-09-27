7b9beecb2fabc11e1410ab220a51761eb92f9284
MXMaddux <maxmaddux@gmail.com>
2024-09-27 06:37:51 -0700

    Third commit

---
+++ src/Test.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+
+const Test = () => {
+  return <div>Test</div>;
+};
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import bookImg from "../src/assets/img/book1.jpg";
+import bookImg2 from "../src/assets/img/book2.jpg";
 
-function Greeting() {
-  return <h2>My First Component</h2>;
-}
+// function Greeting() {
+//   return (
+//     <>
+//       <Person />
+//       <Message />
+//     </>
+//   );
+// }
+
+// function Message() {
+//   return <h2>My message</h2>;
+// }
+
+// const Person = () => {
+//   return <h2>John Doe</h2>;
+// };
+
+const books = [
+  {
+    title:
+      "Ms. Rachel and the Special Surprise: Encouraging Speech and Learning Through Play and Music",
+    author: "Ms Rachel",
+    img: bookImg,
+    id: 1,
+  },
+  {
+    title:
+      "Good Energy: The Surprising Connection Between Metabolism and Limitless Health",
+    author: "Casey Means MD (Author), Calley Means (Author)",
+    img: bookImg2,
+    id: 2,
+  },
+];
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        const { title, img, author } = book;
+        return <Book title={title} img={img} author={author} />;
+      })}
+    </section>
+  );
+};
+
+const Book = ({ img, title, author }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(
+  <>
+    {/* <Greeting /> */}
+    {/* <Message /> */}
+    <BookList />
+  </>
+);
